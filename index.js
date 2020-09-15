0e81f7625d2f8679b178806276b86e8e4c4a5966
spazdrum <spazdrum@gmail.com>
2020-09-14 22:14:48 -0400

    added server on index.js

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const express = require("express");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
